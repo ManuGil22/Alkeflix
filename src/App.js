732bbd3529fdb002eef8db7246052ec1e517d1ab
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-06-27 11:45:18 -0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import './Stylesheets/App.css';
 
 // Components
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <main className="App">
       <Header />
+
+      <Footer />
     </main>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+// Libraries
+import React from "react";
+
+// Stylesheets
+import '../Stylesheets/Footer.css';
+
+function Footer(){
+    return(
+        <div className='footer-container'>
+            <p>© Created by Manuel Gil</p>
+        </div>
+    )
+}
+
+export default Footer;
