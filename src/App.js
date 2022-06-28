181025973782a4aca0dfbc4c21e5d8a47ac34d37
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-06-28 17:09:00 -0300

    made the main section as a component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Libraries
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 
 // Stylesheets
 import './Stylesheets/App.css';
@@ -8,19 +8,18 @@ import './Stylesheets/App.css';
 // Components
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import MainSection from "./Components/MainSection";
 
 function App() {
   return (
     <main className="App">
       <Header />
 
-      <div className="main-section">
-        <div className="main-welcome">
-          <p className="main-welcome-header">WELCOME TO ALKEFLIX!</p>
-          <p className="main-welcome-text">Checkout all the movies in our theaters and</p>
-          <p className="main-welcome-text">never lose a movie premiere again!</p>
-          <Link to="/login" className="main-welcome-btn">Start now!</Link>
-        </div>
+
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<MainSection />} />
+        </Routes>
       </div>
 
       <Footer />
---
+++ src/Components/MainSection.js
@@ -0,0 +1,21 @@
+// Libraries
+import React from "react";
+import { Link } from 'react-router-dom';
+
+// Stylesheets
+import '../Stylesheets/MainSection.css';
+
+function MainSection(){
+    return(
+        <div className="main-section">
+            <div className="main-welcome">
+                <p className="main-welcome-header">WELCOME TO ALKEFLIX!</p>
+                <p className="main-welcome-text">Checkout all the movies in our theaters and</p>
+                <p className="main-welcome-text">never lose a movie premiere again!</p>
+                <Link to="/login" className="main-welcome-btn">Start now!</Link>
+            </div>
+        </div>
+    )
+}
+
+export default MainSection;
